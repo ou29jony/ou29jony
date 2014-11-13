a68349763ba6bfac670cfeb38caab6cf9de8f147
jony <david212_2@mail.ru>
2014-11-13 20:03:42 +0100

    seite wächsel

--- src/view1/view1.js
+++ src/view1/view1.js
@@ -16,18 +16,49 @@ angular.module('myApp.view1', ['ngRoute'])
 }).when('/view1/kontakts', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
+}).when('/view1/:nummber', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
 });
 }])
 .controller('View1Ctrl',function($scope,service) {
-  $scope.array=[];
+   $scope.articleId=1;
+  $scope.nummber=7;
+      $scope.array=[];
       $scope.addMessage=function(message){
       $scope.array.push(message);
       service.addMessage(message);
    };
- 
-   $scope.newarray = service.getArray();
+  $scope.newarray = service.getArray();
    $scope.game = "Hallo World";
+
    $scope.setId=function(id){
+    console.log(id);
   $scope.articleId=id;
+
  };
+
+ $scope.minusId=function(id){
+  console.log(id);
+if($scope.articleId> 5)
+   $scope.articleId=id-5;
+else
+  if($scope.articleId== 4)
+    $scope.articleId=id-4;
+  else
+    if($scope.articleId> 3)
+       $scope.articleId=id-3;
+     else
+      if($scope.articleId> 2)
+         $scope.articleId=id-2;
+       else
+        if($scope.articleId> 1)
+           $scope.articleId=id-1;
+ };
+ $scope.pliusId=function(id){
+  console.log(id);
+if(id<50)
+   $scope.articleId=id+5;
+ };
+ 
 });
